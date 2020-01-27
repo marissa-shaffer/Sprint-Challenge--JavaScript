abc55fd1fc0b595ed1ead93616b9b715ecbbfb9a
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-01-26 20:46:48 -0600

    completed step 2 of prototypes.js

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -6,7 +6,7 @@
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
 function CuboidMaker (length, width, height){
-  this.length = legth;
+  this.length = length;
   this.width = width;
   this.height = height;
 }
@@ -16,7 +16,9 @@ function CuboidMaker (length, width, height){
   
   Formula for cuboid volume: length * width * height
 */
-
+CuboidMaker.prototype.volume = function (){
+  return this.length * this.width * this.height;
+}
 
 /* == Step 3: Surface Area Method ==
   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
