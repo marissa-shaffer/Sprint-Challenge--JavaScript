4f857903e9e0e932b29cc606f260fe7b8767a3d0
Marissa Shaffer <mas@edney.local>
2020-02-05 11:56:19 -0600

    Answered Request 2 of advanced array methods

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -132,7 +132,9 @@ console.log(displayNames);
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(names){
+  return names.animal_name.toLowerCase(); 
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
