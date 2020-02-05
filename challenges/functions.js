eaf3097aa13ecd08706dade402551fe7c05ca00b
Marissa Shaffer <mas@edney.local>
2020-02-05 13:08:19 -0600

    Solved Request 4 of advanced array methods

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,7 +7,7 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 function consume (a, b, cb){
- return callback (cb(a, b));
+ return cb(a, b);
 }
 
 /* Step 2: Create several functions to callback with consume();
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -148,9 +148,10 @@ console.log(lowPopulationAnimals);
 /* Request 4: .reduce() 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 */
-const populationTotal = 0;
-console.log(populationTotal);
-
+const populationTotal = zooAnimals.reduce(function(accumulator, item){
+  return accumulator + item.population; 
+  },0)
+  console.log(populationTotal);
 
 /*
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
